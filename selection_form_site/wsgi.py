ffd3836c84f07855729519561d329e3b593cbac3
Johnny Zimmer <johnmark@zimmerfamily.org>
2018-11-01 09:50:57 -0400

    Carolina Homes selection web app, first commit

---
+++ selection_form_site/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for selection_form_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'selection_form_site.settings')
+
+application = get_wsgi_application()
